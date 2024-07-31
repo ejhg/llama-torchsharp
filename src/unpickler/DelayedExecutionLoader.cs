45b12055c5383fa37d726ad39f355b051164697d
Eugenio De Hoyos <eugene@ketchep.com>
2024-07-31 17:40:39 -0500

    Clean up syntax

--- src/unpickler/DelayedExecutionLoader.cs
+++ src/unpickler/DelayedExecutionLoader.cs
@@ -10,67 +10,64 @@ static class DelayedExecutionLoader
     ) {
         using FileStream fileStream = File.OpenRead (location);
 
-        using (torch.NewDisposeScope ()) {
-            using (torch.no_grad ()) {
-                var hashtable = DelayedExecutionUnpickler.UnpickleStateDict (fileStream, leaveOpen: true);
-                Dictionary<string, Func<object[]>> source = new();
+        using var scope = torch.NewDisposeScope ();
+        using var no_grad = torch.no_grad ();
 
-                foreach (string key in hashtable.Keys) {
-                    source.Add (key, (Func<object[]>)hashtable[key]);
-                }
+        var hashtable = DelayedExecutionUnpickler.UnpickleStateDict (fileStream, leaveOpen: true);
+        Dictionary<string, Func<object[]>> source = new();
 
-                load_state_dict (module, source);
-
-                fileStream.Close ();
-            }
+        foreach (string key in hashtable.Keys) {
+            source.Add (key, (Func<object[]>)hashtable[key]);
         }
+
+        load_state_dict (module, source);
+
+        fileStream.Close ();
     }
 
     static void load_state_dict (
         torch.nn.Module module,
         Dictionary<string, Func<object[]>> source
     ) {
-        var dictionary = module.state_dict ();
+        var state_dict = module.state_dict ();
 
-        using (torch.no_grad ()) {
-            foreach (string key in source.Keys) {
-                if (!dictionary.ContainsKey (key)) {
-                    continue;
-                }
-
-                var args = source[key] ();
-
-                var storageOffset = (int)args[1];
-                var shape = ((object[])args[2])
-                    .Select ((Func<object, long>)(i => (int)i))
-                    .ToArray ();
-                var stride = ((object[])args[3])
-                    .Select ((Func<object, long>)(i => (int)i))
-                    .ToArray ();
-
-                var tObject = ((DelayedExecutionUnpickler.TensorStream)args[0]);
-
-                using var stream = tObject.data;
-
-                if (tObject.dtype == dictionary[key].dtype) {
-                    // read directly into target.
-                    var target = dictionary[key];
-                    target
-                        .as_strided (shape, stride, storageOffset)
-                        .ReadBytesFromStream (stream);
-                    stream.Close ();
-                } else {
-                    // type conversion required. load onto cpu first before copying to target.
-                    using torch.Tensor temp = torch
-                        .empty (shape, tObject.dtype, device: torch.CPU)
-                        .as_strided (shape, stride, storageOffset);
-                    temp.ReadBytesFromStream (stream);
-                    stream.Close ();
-                    dictionary[key].copy_ (temp);
-                }
-
-                Console.WriteLine ($"loading {key} [{string.Join (",", shape)}] {tObject.dtype} -> {dictionary[key].dtype}");
+        foreach (string key in source.Keys) {
+            if (!state_dict.ContainsKey (key)) {
+                continue;
             }
+
+            var args = source[key] ();
+
+            var storageOffset = (int)args[1];
+            var shape = ((object[])args[2])
+                .Select ((Func<object, long>)(i => (int)i))
+                .ToArray ();
+            var stride = ((object[])args[3])
+                .Select ((Func<object, long>)(i => (int)i))
+                .ToArray ();
+
+            var tObject = ((DelayedExecutionUnpickler.TensorStream)args[0]);
+
+            using var stream = tObject.data;
+
+            if (tObject.dtype == state_dict[key].dtype) {
+                // read directly into target.
+                var target = state_dict[key];
+                target
+                    .as_strided (shape, stride, storageOffset)
+                    .ReadBytesFromStream (stream);
+                stream.Close ();
+            } else {
+                // type conversion required. load onto cpu first before copying to target.
+                using torch.Tensor temp = torch
+                    .empty (shape, tObject.dtype, device: torch.CPU)
+                    .as_strided (shape, stride, storageOffset);
+                temp.ReadBytesFromStream (stream);
+                stream.Close ();
+                state_dict[key].copy_ (temp);
+            }
+
+            Console.WriteLine ($"loading {key} [{string.Join (",", shape)}] {tObject.dtype} -> {state_dict[key].dtype}");
         }
     }
 }
